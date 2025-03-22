2c411029c2197df1eff6a192049aa38c9103ae1b
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2025-03-22 11:15:34 -0300

    feat: criacao de anotação de log de saida e estruturacao

--- src/main/java/br/com/mr/aspect_log/AspectLogApplication.java
+++ src/main/java/br/com/mr/aspect_log/AspectLogApplication.java
@@ -1,13 +1,25 @@
 package br.com.mr.aspect_log;
 
+import br.com.mr.aspect_log.testelocal.Teste;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-public class AspectLogApplication {
+public class AspectLogApplication implements CommandLineRunner {
+
+	@Autowired
+	Teste teste;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AspectLogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		teste.soma(10, 10);
+		teste.subtracao(40, 15);
+	}
 }
