de256dbd87bb3f3cdd26cc1df382d71df409f7b8
Wallace Martins <martinsswallace@gmail.com>
2025-04-27 12:47:34 -0300

    feat: criação do log de entrada

--- src/main/java/br/com/mr/aspect_log/testelocal/Teste.java
+++ src/main/java/br/com/mr/aspect_log/testelocal/Teste.java
@@ -1,16 +1,20 @@
 package br.com.mr.aspect_log.testelocal;
 
+import br.com.mr.aspect_log.annotations.method.LogMethodEntry;
 import br.com.mr.aspect_log.annotations.method.LogMethodExit;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Teste {
 
+    @LogMethodEntry(message = "INICIANDO A OPERAÇÃO")
     @LogMethodExit(message = "SOMA REALIZADA COM SUCESSO")
     public int soma(int numero1, int numero2) {
         return numero1 + numero2;
     }
 
+    @LogMethodEntry(message = "INICIANDO A OPERAÇÃO")
+    @LogMethodExit(message = "SUBTRAÇÃO REALIZADA COM SUCESSO")
     public int subtracao(int numero1, int numero2) {
         return numero1 - numero2;
     }
